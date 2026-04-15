6ac1fbfa175e2e43718a6bf6e56eba605c099d9f
NahtanDevFS <jonathan04franco@gmail.com>
2026-04-14 22:35:59 -0600

    corrección de errores menores

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -4,6 +4,7 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true",
   },
 });
 
